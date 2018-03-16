80d51e3eb5b7f94f98cf5015f0301d90115e28af
hilmasha <35945313+hilmasha@users.noreply.github.com>
2018-03-15 20:02:06 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,10 +2,10 @@
 
  //starts a session for a new user
 session_start();
-$dbhost = 'mansci-db.uwaterloo.ca';
-$dbuser = 'hmasha';
-$dbpassword = 'database';
-$dbname = 'hmasha_project';
+$dbhost = 'http://ec2-54-166-222-59.compute-1.amazonaws.com';
+$dbuser = 'root';
+$dbpassword = 'user';
+$dbname = 'custombeauty';
 $conn=mysql_connect($dbhost, $dbuser, $dbpassword) or die("MySQL Error: " . mysql_error());
 mysql_select_db($dbname) or die("MySQL Error: " . mysql_error());
 ?>
