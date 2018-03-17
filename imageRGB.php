205ede3428c795768debdf7f09f75a3fd9488b53
hilmasha <35945313+hilmasha@users.noreply.github.com>
2018-03-17 10:09:33 -0400

    png to jpeg

--- imageRGB.php
+++ imageRGB.php
@@ -3,7 +3,7 @@
 <body>
 
 <?php
-$im = imagecreatefrompng("Face.jpg");
+$im = imagecreatefromjpeg("Face.jpg");
 $rgb = imagecolorat($im, 10, 15);
 $r = ($rgb >> 16) & 0xFF;
 $g = ($rgb >> 8) & 0xFF;
